80186021951ae19043f12821d5a717e2a4cd5ea6
Maximos Kaliakatsos - Papakostas <maximos@athenarc.gr>
2022-05-26 16:30:29 +0300

    non zeros offset diffs corrected

--- c10_LSTMs/symbolic_data_processing.py
+++ c10_LSTMs/symbolic_data_processing.py
@@ -52,11 +52,11 @@ class SymbolicInfo:
         prev_offset = 0
         for n in self.flat.notes:
             if isinstance(n, m21.note.Note):
-                if n.offset - prev_offset > 0:
+                if n.offset - prev_offset >= 0:
                     s += '_(' + str(n.pitch.midi) + ',' + str(n.offset - prev_offset) + ',' + str(n.duration.quarterLength) + ')'
                     prev_offset = n.offset
             elif isinstance(n, m21.chord.Chord):
-                if n.offset - prev_offset > 0:
+                if n.offset - prev_offset >= 0:
                     for note in n:
                         s += '_(' + str(note.pitch.midi) + ',' + str(n.offset - prev_offset) + ',' + str(note.duration.quarterLength) + ')'
                     prev_offset = n.offset
@@ -72,11 +72,11 @@ class SymbolicInfo:
             prev_offset = 0
             for n in f.notes:
                 if isinstance(n, m21.note.Note):
-                    if n.offset - prev_offset > 0:
+                    if n.offset - prev_offset >= 0:
                         s += '_(' + str(n.pitch.midi) + ',' + str(n.offset - prev_offset) + ',' + str(n.duration.quarterLength) + ')'
                         prev_offset = n.offset
                 elif isinstance(n, m21.chord.Chord):
-                    if n.offset - prev_offset > 0:
+                    if n.offset - prev_offset >= 0:
                         for note in n:
                             s += '_(' + str(note.pitch.midi) + ',' + str(n.offset - prev_offset) + ',' + str(note.duration.quarterLength) + ')'
                         prev_offset = n.offset  
